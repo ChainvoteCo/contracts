fb02701c39be7813ffb869f9b113115b9a469690
prasad-kumkar <pk111_@live.com>
2021-11-21 01:21:52 +0530

    election interface initialized

---
+++ contracts/interfaces/IElection.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { IAccessControl } from "@openzeppelin/contracts/access/IAccessControl.sol";
+
+interface IOrganization is IAccessControl {
+}
